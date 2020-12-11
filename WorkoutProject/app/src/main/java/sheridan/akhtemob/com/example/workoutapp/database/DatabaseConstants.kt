e206d11f7321500eabba76cf8223ca4d8b8c098f
MobinAkhter <55329336+MobinAkhter@users.noreply.github.com>
2020-12-11 14:01:56 -0500

    Delete DatabaseConstants.kt

--- WorkoutProject/app/src/main/java/sheridan/akhtemob/com/example/workoutapp/database/DatabaseConstants.kt
+++
@@ -1,9 +0,0 @@
-package sheridan.akhtemob.com.example.workoutapp.database
-
-object DatabaseConstants {
-
-    const val databaseName = "workout.app.database.v1"
-    const val tableJoggingEntries = "tableJoggingEntries"
-    const val tableFreeWeightsEntries = "tableFreeWeightsEntries"
-
-}
